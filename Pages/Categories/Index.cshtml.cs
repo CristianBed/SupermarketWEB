904dfc0ed79a9ab0de1aeb2f3f28b6e399466bef
CristianBed <162656608+CristianBed@users.noreply.github.com>
2025-05-17 13:26:11 -0500

    actualizacion proyecto

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        /*private readonly SupermarketContext _Context;
+        public IndexModel(SumpermarketContext context)
+        {
+            _Context = context;
+        }
+        public IList<Category> Categories { get; set; } = default!;
+
+        public async Task OneGetAsync()
+        {
+            if (_Context.Categories != null)
+            {
+                Categories = await _Context.Categories.ToListAsync();
+            }
+        }
+        */
+    }
+        
+}
+
+
+
