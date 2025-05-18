7b872cbe549ff62509ec936277a57db459d9191b
CristianBed <162656608+CristianBed@users.noreply.github.com>
2025-05-18 18:28:43 -0500

    Configuracion create paymode

---
+++ Pages/PayMode/Create.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperMarketWEB.Data;
+using SuperMarketWEB.Models;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+    [Authorize]
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.PayModes == null || PayMode == null)
+            {
+                return Page();
+            }
+            _context.PayModes.Add(PayMode);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
